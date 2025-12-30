b3958560881389e7e9dae93152d6973df9197dbb
leerobertdyer <leerobertdyer@gmail.com>
2025-12-30 13:14:07 -0500

    healthcheck and static public folder up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import 'dotenv/config'
+import "dotenv/config";
+import path from 'path'
 
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT;
 const app = express();
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(express.static(path.join(__dirname, "public")))
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
